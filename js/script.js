3e2b4a24732e411e1ef049ab1a12467f57913820
Mohammad Anwar Shah <mohammadanwarshah@gmail.com>
2016-06-21 00:46:15 +0600

    adds basic function to input digits, remove, reset

--- js/script.js
+++ js/script.js
@@ -1,69 +1,34 @@
-function extractSnippets(searchResult) {
-  var html = '';
-
-  var page, title, snippet;
-
-  for (var pageid in searchResult) {
-    page = searchResult[pageid];
-    title = page.title;
-    snippet = page.extract;
-    html += ('<a class="divLink" href="http://en.wikipedia.org/?curid='+pageid+'" target="_blank"><div class="preview"><h3>'+ title + '</h3><p>' + snippet + '</p></div></a>');
-  }
-  return html;
+function appendToDisplay(display, text) {
+  display.val( display.val() + text );
 };
 
-function fetchData(searchedTerm) {
-  return $.ajax({
-    url: 'http://en.wikipedia.org/w/api.php',
-    data: {
-      action: 'query',
-      prop: 'extracts',
-      generator: 'search',
-      gsrsearch: searchedTerm,
-      exsentences: 1,
-      exlimit: 10,
-      exintro: 1,
-      explaintext: 1,
-      gsrnamespace: 0,
-      gsrlimit: 10,
-      format: 'json',
-    },
-    dataType: 'jsonp',
-    success: function (x) {
-      console.log(x);
-
-      var html = extractSnippets(x.query.pages);
-      $('div#result').html(html).show().fadeIn('fast');
-    }
-  })
-  .done(
-    function () {
-      $("a.divLink")
-      .mouseenter(function () {
-        $(this).addClass('active');
-      })
-      .mouseleave(function () {
-        $(this).removeClass('active');
-      });
-    }
-  );
+function clearDisplay() {
+  $('.display').val('');
 };
 
-$(document).ready(function () {
+function eraseDigit(display) {
+  currentText = display.val();
+  newText = currentText.slice(0, currentText.length - 1);
+  display.val(newText);
+};
 
-  var inputDiv = $('div#input');
-  var resultDiv = $('div#result');
-  var inputField = $('form#inputForm').find('input:first');
+$( document ).ready(function() {
+  var display = $('.display');
 
-  inputDiv.find('form#inputForm').submit(function (e) {
-    e.preventDefault();
-    var value = inputField.val();
-    fetchData(value);
+  $('.digit').click(function(e) {
+    appendToDisplay(display, $(this).text());
   });
 
-  inputDiv.find('button#clearBtn').on('click', function () {
-    resultDiv.html('');
-    inputField.val('');
-    resultDiv.fadeOut('slow');
+  $('.operator').click(function(e) {
+    appendToDisplay(display, $(this).text());
   });
+
+  $('#ac').click(function(e) {
+    clearDisplay();
+  });
+
+  $('#ce').click(function(e) {
+    eraseDigit(display);
+  });
+
 });
